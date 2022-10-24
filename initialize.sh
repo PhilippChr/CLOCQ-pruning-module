157c7e377797d4a6ff5cb5b0f9333e535cfe6732
Philipp Christmann <pchristm@mpi-inf.mpg.de>
2022-10-24 18:10:48 +0200

    minor fix

--- initialize.sh
+++ initialize.sh
@@ -6,6 +6,6 @@ mkdir -p results
 mkdir -p out
 
 # download 
-wget https://qa.mpi-inf.mpg.de/clocq/smart/SMART2022-EL-wikidata-train.json
-wget https://qa.mpi-inf.mpg.de/clocq/smart/SMART2022-EL-wikidata-test.json
-wget https://qa.mpi-inf.mpg.de/clocq/smart/model.bin
+wget https://qa.mpi-inf.mpg.de/clocq/smart/SMART2022-EL-wikidata-train.json -P data/
+wget https://qa.mpi-inf.mpg.de/clocq/smart/SMART2022-EL-wikidata-test.json -P data/
+wget https://qa.mpi-inf.mpg.de/clocq/smart/model.bin -P data/
